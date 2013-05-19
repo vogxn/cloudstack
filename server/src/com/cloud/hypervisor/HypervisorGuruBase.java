vogxn/cloudstack

commit 23e54bb0f4c64735bb34413ea26ea1804c846ff5

Cloudstack-711: Cpu and Ram Overcommit Ratio.

server/src/com/cloud/hypervisor/HypervisorGuruBase.java
@@ -76,9 +76,11 @@ public abstract class HypervisorGuruBase extends AdapterBase implements Hypervis
 
         ServiceOffering offering = vmProfile.getServiceOffering();  
         VirtualMachine vm = vmProfile.getVirtualMachine();
-
-        VirtualMachineTO to = new VirtualMachineTO(vm.getId(), vm.getInstanceName(), vm.getType(), offering.getCpu(), offering.getSpeed(), 
-                offering.getRamSize() * 1024l * 1024l, offering.getRamSize() * 1024l * 1024l, null, null, vm.isHaEnabled(), vm.limitCpuUse(), vm.getVncPassword());
+        Long minMemory = (long) (offering.getRamSize()/vmProfile.getCpuOvercommitRatio());
+        int  minspeed= (int)(offering.getSpeed()/vmProfile.getMemoryOvercommitRatio());
+        int  maxspeed = (offering.getSpeed());
+        VirtualMachineTO to = new VirtualMachineTO(vm.getId(), vm.getInstanceName(), vm.getType(), offering.getCpu(), minspeed, maxspeed,
+                minMemory * 1024l * 1024l, offering.getRamSize() * 1024l * 1024l, null, null, vm.isHaEnabled(), vm.limitCpuUse(), vm.getVncPassword());
         to.setBootArgs(vmProfile.getBootArgs());
 
         List<NicProfile> nicProfiles = vmProfile.getNics();

api/src/com/cloud/agent/api/to/VirtualMachineTO.java
@@ -28,7 +28,8 @@ public class VirtualMachineTO {
     private BootloaderType bootloader;
     Type type;
     int cpus;
-    Integer speed;
+    Integer minSpeed;
+    Integer maxSpeed;
     long minRam;
     long maxRam;
     String hostName;
@@ -47,12 +48,13 @@ public class VirtualMachineTO {
     VolumeTO[] disks;
     NicTO[] nics;
 
-    public VirtualMachineTO(long id, String instanceName, VirtualMachine.Type type, int cpus, Integer speed, long minRam, long maxRam, BootloaderType bootloader, String os, boolean enableHA, boolean limitCpuUse, String vncPassword) {
+    public VirtualMachineTO(long id, String instanceName, VirtualMachine.Type type, int cpus, Integer minSpeed, Integer maxSpeed, long minRam, long maxRam, BootloaderType bootloader, String os, boolean enableHA, boolean limitCpuUse, String vncPassword) {
         this.id = id;
         this.name = instanceName;
         this.type = type;
         this.cpus = cpus;
-        this.speed = speed;
+        this.minSpeed = minSpeed;
+        this.maxSpeed = maxSpeed;
         this.minRam = minRam;
         this.maxRam = maxRam;
         this.bootloader = bootloader;
@@ -101,10 +103,13 @@ public class VirtualMachineTO {
         this.cpus = cpus;
     }
 
-    public Integer getSpeed() {
-        return speed;
+    public Integer getMinSpeed() {
+        return minSpeed;
     }
 
+    public Integer getMaxSpeed() {
+        return maxSpeed;
+    }
     public boolean getLimitCpuUse() {
     	return limitCpuUse;
     }

api/src/com/cloud/resource/ResourceService.java
@@ -71,7 +71,7 @@ public interface ResourceService {
 
     boolean deleteCluster(DeleteClusterCmd cmd);
 
-    Cluster updateCluster(Cluster cluster, String clusterType, String hypervisor, String allocationState, String managedstate);
+    Cluster updateCluster(Cluster cluster, String clusterType, String hypervisor, String allocationState, String managedstate, Float memoryOvercommitRaito, Float cpuOvercommitRatio);
 
     List<? extends Host> discoverHosts(AddHostCmd cmd) throws IllegalArgumentException, DiscoveryException, InvalidParameterValueException;
 

api/src/com/cloud/vm/VirtualMachineProfile.java
@@ -136,4 +136,10 @@ public interface VirtualMachineProfile<T extends VirtualMachine> {
     BootloaderType getBootLoaderType();
 
     Map<Param, Object> getParameters();
+
+    Float getCpuOvercommitRatio();
+
+    Float getMemoryOvercommitRatio();
+
+
 }

api/src/org/apache/cloudstack/api/ApiConstants.java
@@ -46,6 +46,7 @@ public class ApiConstants {
     public static final String COMPONENT = "component";
     public static final String CPU_NUMBER = "cpunumber";
     public static final String CPU_SPEED = "cpuspeed";
+    public static final String CPU_OVERCOMMIT_RATIO="cpuovercommitratio";
     public static final String CREATED = "created";
     public static final String CUSTOMIZED = "customized";
     public static final String DESCRIPTION = "description";
@@ -119,6 +120,7 @@ public class ApiConstants {
     public static final String MAX = "max";
     public static final String MAX_SNAPS = "maxsnaps";
     public static final String MEMORY = "memory";
+    public static final String MEMORY_OVERCOMMIT_RATIO="memoryovercommitratio";
     public static final String MODE = "mode";
     public static final String NAME = "name";
     public static final String METHOD_NAME = "methodname";

api/src/org/apache/cloudstack/api/command/admin/cluster/AddClusterCmd.java
@@ -20,6 +20,10 @@ package org.apache.cloudstack.api.command.admin.cluster;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.cloud.exception.InvalidParameterValueException;
+import org.apache.cloudstack.api.*;
+import org.apache.log4j.Logger;
+
 import org.apache.cloudstack.api.APICommand;
 import org.apache.cloudstack.api.ApiConstants;
 import org.apache.cloudstack.api.ApiErrorCode;
@@ -80,6 +84,12 @@ public class AddClusterCmd extends BaseCmd {
 
     @Parameter(name = ApiConstants.VSM_IPADDRESS, type = CommandType.STRING, required = false, description = "the ipaddress of the VSM associated with this cluster")
     private String vsmipaddress;
+
+    @Parameter (name=ApiConstants.CPU_OVERCOMMIT_RATIO, type = CommandType.STRING, required = false , description = "value of the cpu overcommit ratio, defaults to 1")
+    private String  cpuovercommitRatio;
+
+    @Parameter(name = ApiConstants.MEMORY_OVERCOMMIT_RATIO, type = CommandType.STRING, required = false ,description = "value of the default ram overcommit ratio, defaults to 1")
+    private String  memoryovercommitratio;
 
     public String getVSMIpaddress() {
         return vsmipaddress;
@@ -147,9 +157,26 @@ public class AddClusterCmd extends BaseCmd {
         this.allocationState = allocationState;
     }
 
+    public Float getCpuOvercommitRatio (){
+        if(cpuovercommitRatio != null){
+           return Float.parseFloat(cpuovercommitRatio);
+        }
+        return 1.0f;
+    }
+
+    public Float getMemoryOvercommitRaito (){
+        if (memoryovercommitratio != null){
+            return Float.parseFloat(memoryovercommitratio);
+        }
+        return 1.0f;
+    }
+
     @Override
     public void execute(){
         try {
+            if ((getMemoryOvercommitRaito().compareTo(1f) < 0) | (getCpuOvercommitRatio().compareTo(1f) < 0)) {
+                throw new InvalidParameterValueException("Cpu and ram overcommit ratios  should not be less than 1");
+            }
             List<? extends Cluster> result = _resourceService.discoverCluster(this);
             ListResponse<ClusterResponse> response = new ListResponse<ClusterResponse>();
             List<ClusterResponse> clusterResponses = new ArrayList<ClusterResponse>();

api/src/org/apache/cloudstack/api/command/admin/cluster/UpdateClusterCmd.java
@@ -54,6 +54,13 @@ public class UpdateClusterCmd extends BaseCmd {
     @Parameter(name=ApiConstants.MANAGED_STATE, type=CommandType.STRING, description="whether this cluster is managed by cloudstack")
     private String managedState;
 
+    @Parameter(name=ApiConstants.CPU_OVERCOMMIT_RATIO, type = CommandType.STRING, description = "Value of cpu overcommit ratio")
+    private String cpuovercommitratio;
+
+    @Parameter(name=ApiConstants.MEMORY_OVERCOMMIT_RATIO, type = CommandType.STRING, description = "Value of ram overcommit ratio")
+    private String memoryovercommitratio;
+
+
     public String getClusterName() {
         return clusterName;
     }
@@ -100,6 +107,20 @@ public class UpdateClusterCmd extends BaseCmd {
         this.managedState = managedstate;
     }
 
+    public Float getCpuOvercommitRatio (){
+        if(cpuovercommitratio != null){
+            return Float.parseFloat(cpuovercommitratio);
+        }
+        return 1.0f;
+    }
+
+    public Float getMemoryOvercommitRaito (){
+        if (memoryovercommitratio != null){
+            return Float.parseFloat(memoryovercommitratio);
+        }
+        return 1.0f;
+    }
+
     @Override
     public void execute(){
         Cluster cluster = _resourceService.getCluster(getId());
@@ -107,7 +128,11 @@ public class UpdateClusterCmd extends BaseCmd {
             throw new InvalidParameterValueException("Unable to find the cluster by id=" + getId());
         }
 
-        Cluster result = _resourceService.updateCluster(cluster, getClusterType(), getHypervisor(), getAllocationState(), getManagedstate());
+        if ((getMemoryOvercommitRaito().compareTo(1f) < 0) | (getCpuOvercommitRatio().compareTo(1f) < 0)) {
+            throw new InvalidParameterValueException("Cpu and ram overcommit ratios  should be greater than one");
+        }
+
+        Cluster result = _resourceService.updateCluster(cluster, getClusterType(), getHypervisor(), getAllocationState(), getManagedstate(), getMemoryOvercommitRaito(), getCpuOvercommitRatio());
         if (result != null) {
                 ClusterResponse clusterResponse = _responseGenerator.createClusterResponse(cluster, false);
                 clusterResponse.setResponseName(getCommandName());

api/src/org/apache/cloudstack/api/response/ClusterResponse.java
@@ -62,6 +62,12 @@ public class ClusterResponse extends BaseResponse {
     @SerializedName("capacity")  @Param(description="the capacity of the Cluster", responseObject = CapacityResponse.class)
     private List<CapacityResponse> capacitites;
 
+    @SerializedName("cpuovercommitratio") @Param(description = "The cpu overcommit ratio of the cluster")
+    private String cpuovercommitratio;
+
+    @SerializedName("memoryovercommitratio") @Param (description = "The ram overcommit ratio of the cluster")
+    private String memoryovercommitratio;
+
     public String getId() {
         return id;
     }
@@ -148,5 +154,19 @@ public class ClusterResponse extends BaseResponse {
 
     public void setCapacitites(ArrayList<CapacityResponse> arrayList) {
         this.capacitites = arrayList;
+    }
+    public void setCpuovercommitratio(String cpuovercommitratio){
+        this.cpuovercommitratio= cpuovercommitratio;
+    }
+    public void setRamovercommitratio (String memoryOvercommitRatio){
+        this.memoryovercommitratio= memoryOvercommitRatio;
+    }
+
+    public String getCpuovercommitratio (){
+        return cpuovercommitratio;
+    }
+
+    public String getRamovercommitratio (){
+        return memoryovercommitratio;
     }
 }

commit eb65cf0868d7078fe38a5d4206fcd89259396cd9

CLOUDSTACK-2554: Incorrect compute of minmemory and cpu
23e54bb0 introduced multiple hypervisors support for cpu and memory
overcommit. Here the HypervisorGuru base which determines the min, max
range for the memory for all hypervisors computes the minCpu using the
MemoryOverCommit ratio and minMemory using the CpuOverCommit ratio.

Minor typo/logic issue but massive damage across all HV if enabled ;)

Signed-off-by: Prasanna Santhanam <tsp@apache.org>

server/src/com/cloud/hypervisor/HypervisorGuruBase.java
@@ -86,9 +86,9 @@ public abstract class HypervisorGuruBase extends AdapterBase implements Hypervis
 
         ServiceOffering offering = vmProfile.getServiceOffering();  
         VirtualMachine vm = vmProfile.getVirtualMachine();
-        Long minMemory = (long) (offering.getRamSize()/vmProfile.getCpuOvercommitRatio());
-        int  minspeed= (int)(offering.getSpeed()/vmProfile.getMemoryOvercommitRatio());
-        int  maxspeed = (offering.getSpeed());
+        Long minMemory = (long) (offering.getRamSize() / vmProfile.getMemoryOvercommitRatio());
+        int minspeed = (int) (offering.getSpeed() / vmProfile.getCpuOvercommitRatio());
+        int maxspeed = (offering.getSpeed());
         VirtualMachineTO to = new VirtualMachineTO(vm.getId(), vm.getInstanceName(), vm.getType(), offering.getCpu(), minspeed, maxspeed,
                 minMemory * 1024l * 1024l, offering.getRamSize() * 1024l * 1024l, null, null, vm.isHaEnabled(), vm.limitCpuUse(), vm.getVncPassword());
         to.setBootArgs(vmProfile.getBootArgs());